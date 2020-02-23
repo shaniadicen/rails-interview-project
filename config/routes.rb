7bb44c327e6e5dfb10f187da70c5abd5cffc15b2
Shania Dicen <shaniadicendev@gmail.com>
2020-02-24 03:49:34 +0800

    Add routes index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "welcome/index"
-
   root "welcome#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :questions, only: [:index, :show]
+    end
+  end
 end
