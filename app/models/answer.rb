352150f2149d5c5b10c270c03d2da3e1918c16f4
Shania Dicen <shaniadicendev@gmail.com>
2020-02-23 19:05:00 +0800

    Fix code format with rubocop to enforce rails coding standard

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,4 @@
 class Answer < ActiveRecord::Base
-
   belongs_to :question
   belongs_to :user
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-
-  has_many :questions, inverse_of: :asker
-  has_many :answers,   inverse_of: :answerer
-
+  has_many :questions, dependent: :destroy, inverse_of: :asker
+  has_many :answers, dependent: :destroy, inverse_of: :answerer
 end
