e83f7a13dd7ad6ffc59f337cb731702d6c826e7b
Shania Dicen <shaniadicendev@gmail.com>
2020-02-24 03:49:20 +0800

    Add authorize api_key in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def authorize
+    @tenant = Tenant.find_by(api_key: params[:api_key])
+
+    render json: { errors: ["Invalid API Key"] }, status: :unauthorized if @tenant.nil?
+  end
 end
