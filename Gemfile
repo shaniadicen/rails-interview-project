3fe2ff0c8879dcc85186b759490a82528f4384ba
Shania Dicen <shaniadicendev@gmail.com>
2020-02-24 02:19:24 +0800

    Add active model serializer gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
+gem "active_model_serializers"
 gem "coffee-rails"
 gem "jquery-rails"
 gem "rails", "5.2.3"
---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,9 @@
+module Api
+  module V1
+    class QuestionsController < ApplicationController
+      def index; end
+
+      def show; end
+    end
+  end
+end
